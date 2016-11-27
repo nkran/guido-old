cf9d51f7fa4fe9335fcc5169e95ec8e849832540
nkran <nace.kranjc@gmail.com>
2016-11-27 23:11:25 +0000

    Remove duplicates and add grouping by deletion

--- mmej.py
+++ mmej.py
@@ -5,9 +5,10 @@ import math
 # TODO
 # - look in negative strand
 # - microhomology, out-of-frame
+# - argv parser - outputs, inputs
 
 # Sequence
-seq = 'CGAGCGCGGGGCAGGTGCCCGCTGGAACTCGCGCCTCGCAGCGCTGGGCGGCCGGGGCCGGGCAGGGTAGTGCGGGAAGATCGGGGTCTGGGGTCGGTGCCGGCGGGACTCCGAAAGGAGGGAGCCGGG'
+seq = 'GATCAGATGCTACGATCGATGTCGCAGCGAGGTGAGGAAGAAAGTGAGGAGGAGGGTGGTAGTGCCACACAGAGAGCTTCGGATG'
 max_flanking_length = 40
 min_flanking_length = 15
 length_weight = 20.0
@@ -17,7 +18,7 @@ length_weight = 20.0
 def find_breaks(sequence):
     breaks_list = []
 
-    pams = [m.start() for m in re.finditer(r'[ACGT]GG', sequence) if m.start(0) - min_flanking_length > 0 and m.end(0) + min_flanking_length < len(sequence)]
+    pams = [m.start() for m in re.finditer(r'AGG', sequence) if m.start(0) - min_flanking_length > 0 and m.end(0) + min_flanking_length < len(sequence)]
     for pam in pams:
 
         break_dict = {}
@@ -65,15 +66,20 @@ def find_microhomologies(left_seq, right_seq):
 
     return kmers
 
-
+# simulates end joining with microhomology
 def simulate_end_joining(breaks_list):
 
     for i, item in enumerate(breaks_list):
 
         # TODO: remove and enable all breaks
-        if i == 0:
+        if i == 4:
             sequence = item['seq']
             br = item['br']
+
+            print sequence
+
+            # create list for storing combinations
+            combination_list = []
 
             # split sequence at the break
             left_seq = sequence[:br]
@@ -96,8 +102,12 @@ def simulate_end_joining(breaks_list):
                 # get combinations
                 pos_combinations = list(itertools.product(left_positions, right_positions))
 
+
                 # generate microhomology for every combination
                 for combination in pos_combinations:
+
+                    # save output to dict
+                    combination_dict = {}
 
                     # left side
                     left_seq_pos = combination[0]
@@ -113,7 +123,7 @@ def simulate_end_joining(breaks_list):
 
                     # score pattern
                     length_factor =  round(1 / math.exp((deletion_length) / (length_weight)), 3)
-                    score_pattern = 100 * length_factor * ((len(pattern) - pattern_GC) + (pattern_GC * 2))
+                    pattern_score = 100 * length_factor * ((len(pattern) - pattern_GC) + (pattern_GC * 2))
 
                     # frame shift
                     if deletion_length % 3 == 0:
@@ -121,11 +131,52 @@ def simulate_end_joining(breaks_list):
                     else:
                         frame_shift = "+"
 
+                    # create dictionary
+                    combination_dict['left'] = left_seq[:left_seq_pos] + '-' * left_deletion_length
+                    combination_dict['left_seq'] = left_seq[:left_seq_pos]
+                    combination_dict['right'] = '+' * right_deletion_length + right_seq[right_seq_pos:]
+                    combination_dict['right_seq'] = right_seq[right_seq_pos:]
+                    combination_dict['pattern'] = pattern
+                    combination_dict['pattern_score'] = pattern_score
+                    combination_dict['deletion_seq'] = deletion_seq
+                    combination_dict['frame_shift'] = frame_shift
+
+                    # add to list
+                    combination_list.append(combination_dict)
+
                     # output
-                    print left_seq[:left_seq_pos] + '-' * left_deletion_length + '+' * right_deletion_length + (right_seq[right_seq_pos:]),
-                    print pattern, deletion_length, pattern_GC, frame_shift, score_pattern
+                    # print left_seq[:left_seq_pos] + '-' * left_deletion_length + '+' * right_deletion_length + (right_seq[right_seq_pos:]), '\t',
+                    # print pattern, '\t', deletion_length,'\t', '\t', frame_shift,'\t', pattern_score,'\t', deletion_seq
+
+            # remove duplicates
+            combination_list = [dict(t) for t in set([tuple(sorted(combination_dict.items())) for combination_dict in combination_list])]
+
+            return combination_list
+
+# groups all combinations of microhomogogies by deletion sequence and sums up the scores for same deletions
+def group_by_deletion(pattern_list, sorting_field = 'score'):
+
+    # group list
+    deletion_groups = []
+
+    for deletion, group in itertools.groupby(sorted(pattern_list, key=lambda x: x['deletion_seq']), key=lambda x: x['deletion_seq']):
+
+        del_group = {}
+        del_group['score'] = 0
+
+        for i, combination in enumerate(group):
+            del_group['score'] += combination['pattern_score']
+
+        del_group['count'] = i + 1
+        del_group['deletion'] = deletion
+
+        deletion_groups.append(del_group)
+
+    for del_group in sorted(deletion_groups, key=lambda x: x[sorting_field], reverse = True):
+        print del_group['score'], '\t', del_group['count'], '\t', del_group['deletion']
+
 
 # run
 break_dict = find_breaks(seq)
-simulate_end_joining(break_dict)
-
+pattern_list = simulate_end_joining(break_dict)
+group_by_deletion(pattern_list)
