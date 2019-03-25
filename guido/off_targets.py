b78ea4abbea901c4f11cedbcc07b362bee81f08f
Thomas Courty <thomas.cour@gmail.com>
2019-03-25 17:17:10 +0000

    Update off_targets.py
    Fixed empty target_dict problem.
    Moved everything downstream a tab. Lmk if there's a better way to do this in the future!

--- guido/off_targets.py
+++ guido/off_targets.py
@@ -48,29 +48,37 @@ def run_bowtie(cut_sites, genome_index_path):
 def off_target_evaluation(cut_sites, target_dict):
 
     logger.info('Evaluating off-targets ...')
-    for tid in sorted(target_dict.keys(), key=lambda x: int(x)):
 
-        targets = target_dict[tid]
-        cut = cut_sites[int(tid)]
-
-        cut_chromosome, cut_start, cut_end = cut['guide_loc']
-        cut.update({'off_targets': {
+    if not target_dict:
+        for cut in cut_sites:
+            cut.update({'off_targets': {
                         'count': [0, 0, 0, 0], # off-targets with 0, 1, 2, 3 mismatches
                         'ot': []
-                    }})
+                        }})
+    else:
+        for tid in sorted(target_dict.keys(), key=lambda x: int(x)):
 
-        for target in targets:
-            # if the hit is not on-target
-            if target['start'] not in range(cut_start - 1, cut_start + 1) and target['chromosome'] != cut_chromosome:
+            targets = target_dict[tid]
+            cut = cut_sites[int(tid)]
 
-                # append information about off-target
-                cut['off_targets']['ot'].append(target)
+            cut_chromosome, cut_start, cut_end = cut['guide_loc']
+            cut.update({'off_targets': {
+                            'count': [0, 0, 0, 0], # off-targets with 0, 1, 2, 3 mismatches
+                            'ot': []
+                        }})
 
-                # split mismatch string by ',' and count the number of mismatches
-                # filter out empty string after splitting
-                missmatches = len(filter(None, target['mismatches'].split(',')))
+            for target in targets:
+                # if the hit is not on-target
+                if target['start'] not in range(cut_start - 1, cut_start + 1) and target['chromosome'] != cut_chromosome:
 
-                # add to the mismatches counter
-                cut['off_targets']['count'][missmatches] += 1
+                    # append information about off-target
+                    cut['off_targets']['ot'].append(target)
+
+                    # split mismatch string by ',' and count the number of mismatches
+                    # filter out empty string after splitting
+                    missmatches = len(filter(None, target['mismatches'].split(',')))
+
+                    # add to the mismatches counter
+                    cut['off_targets']['count'][missmatches] += 1
 
     return cut_sites
