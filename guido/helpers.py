1ae15c45fd5f99262943b2dd78d7d380fdc654b2
tycour <thomas.cour@gmail.com>
2019-11-27 13:38:49 +0000

    Annotation refactor and MP

--- guido/helpers.py
+++ guido/helpers.py
@@ -1,5 +1,6 @@
 import re
 import multiprocessing.pool as mp
+import pandas
 
 def istarmap(self, func, iterable, chunksize=1):
     """
@@ -42,15 +43,36 @@ def rev_comp(seq):
 
 
 def parse_MD_tag(sequence, md_tag):
+    """
+    TODO: if you have 0A -> works fine
+          BUT:
+          if you have 0A0T -> ix doesn't get updated on +1 to a position AFTER 0A
+    """
     tag = md_tag.split(':')[2]
     tag_chain = re.findall("([0-9]+)([AaCcGgTt]?)", tag)
-    
-    string = ''
+    seq_len = len(sequence)
+    seq = ['.'] * seq_len
+    ix = 0
     
     for link in tag_chain:
         dist, base = link
-        
-        string += '.' * int(dist)
-        string += base
+        ix += int(dist)
+        if base:
+            seq[ix] = base
     
-    return string
+    return ''.join(seq)
+
+def geneset_to_pandas(geneset):
+    """
+    Life is a bit easier when a geneset is a pandas DataFrame.
+    """
+    items = []
+
+    for n in geneset.dtype.names:
+        v = geneset[n]
+        # convert bytes columns to unicode (which pandas then converts to object)
+        if v.dtype.kind == 'S':
+            v = v.astype('U')
+        items.append((n, v))
+
+    return pandas.DataFrame.from_dict(dict(items))
--- guido/output.py
+++ guido/output.py
@@ -24,7 +24,7 @@ def render_output(cut_sites, output_folder, targets_df=None):
     cs_df[['chrom', 'start', 'end']] = pd.DataFrame(cs_df['guide_loc'].values.tolist())
     cs_df['variants'] = pd.DataFrame(cs_df['variants'])
 
-    output = cs_df[['chrom', 'start', 'end', 'strand', 'absolute_cut_pos', 'guide', 'cons_score', 'variants_n', 'complete_score', 'sum_score', 'offtargets_str', 'offtargets_n']]
+    output = cs_df[['chrom', 'start', 'end', 'strand', 'absolute_cut_pos', 'guide', 'annotation', 'cons_score', 'variants_n', 'complete_score', 'sum_score', 'offtargets_str', 'offtargets_n']]
     output = output.sort_values(by=['chrom', 'start'])
     output.to_csv(os.path.join(output_folder, 'guides_list.csv'), index=False)
 
